43a769a155d5be6f6422116d44b231f536a2be42
Jequex <chukwunonso.onyejekwe@gmail.com>
2022-06-19 09:34:22 +0100

    finalizing design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finlex-frontend';
+  items = [
+    {logo: 'bi bi-pencil-square', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-arrow-clockwise', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-list-check', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-globe', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-gear', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-people', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-building', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-briefcase', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-newspaper', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-graph-up-arrow', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-currency-euro', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-briefcase', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    {logo: 'bi bi-building', name: 'Lorem Ipsum', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
+  ]
 }
